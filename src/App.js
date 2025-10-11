c2571c7cfdfa18c13d4311cffefd99cec1103794
Chonky_Seal <MatimotTheTimoters@gmail.com>
2025-10-12 04:43:12 +0800

    feat: add routes to app.js

--- src/App.js
+++ src/App.js
@@ -6,42 +6,49 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import NavbarComponent from './components/NavbarComponent';
 import FloatingActionMenu from './components/FloatingActionMenu';
 import FooterComponent from './components/FooterComponent';
-import AsideComponent from './components/AsideComponent';
 
 import LandingPage from './pages/LandingPage';
 import RegistrationPage from './pages/RegistrationPage';
 import LoginPage from './pages/LoginPage';
+import TestPages from './pages/TestPages';
+
 import FeedPage from './pages/FeedPage';
 import DiscoverPage from './pages/DiscoverPage';
 import MyKitchenPage from './pages/MyKitchenPage';
+
 import InventoryPage from './pages/InventoryPage';
-import TestPages from './pages/TestPages';
+import ShopPage from './pages/ShopPage';
+import ProfilePage from './pages/ProfilePage';
+import UsersPage from './pages/UsersPage';
+import SettingsPage from './pages/SettingsPage';
 
 // App.js - Add bottom padding class
 function App() {
   return (
     <Router>
-      <div className="app-container">
-        <NavbarComponent />
-        <main className="main-content with-fixed-footer">
-          <Routes>
-            {/* Homepage routes */}
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/registration" element={<RegistrationPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="*" element={<Navigate to="/" replace />} />
+      <NavbarComponent />
+      <Routes>
+        {/* Main routes */}
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/registration" element={<RegistrationPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
 
-            {/* AsideComponent routes */}
-            <Route path="/feed" element={<FeedPage />} />
-            <Route path="/discover" element={<DiscoverPage />} />
-            <Route path="/my-kitchen" element={<MyKitchenPage />} />
-            <Route path="/inventory" element={<InventoryPage />} />
-            <Route path="/TestPages" element={<TestPages />} />
-          </Routes>
-        </main>
-        <FloatingActionMenu />
-        <FooterComponent />
-      </div>
+        {/* Navbar routes */}
+        <Route path="/inventory" element={<InventoryPage />} />
+        <Route path="/shop" element={<ShopPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/users" element={<UsersPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+
+        {/* AsideComponent routes */}
+        <Route path="/feed" element={<FeedPage />} />
+        <Route path="/discover" element={<DiscoverPage />} />
+        <Route path="/my-kitchen" element={<MyKitchenPage />} />
+        <Route path="/TestPages" element={<TestPages />} />
+      </Routes>
+      <FloatingActionMenu />
+      <FooterComponent />
     </Router>
   );
 }
