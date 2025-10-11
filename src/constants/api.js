e25c8e9bea9e3d965711bf71e4cddbbbfc013b40
Chonky_Seal <MatimotTheTimoters@gmail.com>
2025-10-11 10:11:17 +0800

    chore: add challengesCookQuota as sheet API

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,7 +1,9 @@
 const apiLinks = {
     main: "https://sheetdb.io/api/v1/bry4xqkea1b8c",
     users: "https://sheetdb.io/api/v1/bry4xqkea1b8c?sheet=users",
-    recipes: "https://sheetdb.io/api/v1/bry4xqkea1b8c?sheet=recipes"
+    recipes: "https://sheetdb.io/api/v1/bry4xqkea1b8c?sheet=recipes",
+    challengesCookQuota: "https://sheetdb.io/api/v1/bry4xqkea1b8c?sheet=challenges-cook-quota",
+
 }
 
 export default apiLinks;
