e484b949458283b11dcca132e3d30be3177a563e
Chonky_Seal <MatimotTheTimoters@gmail.com>
2025-10-12 07:20:03 +0800

    feat: create batchId generator method to reduce GET requests

--- src/hooks/uuidHelper.js
+++ src/hooks/uuidHelper.js
@@ -42,3 +42,14 @@ export async function generateUniqueId(sheetUrl, opts = {}) {
   }
   throw new Error('Could not generate a unique id after retries');
 }
+
+// Add to src/hooks/uuidHelper.js
+export const generateBatchIds = async (count, prefix = 'id') => {
+  // Generate IDs locally without API calls
+  const ids = [];
+  for (let i = 0; i < count; i++) {
+    const id = `${prefix}_${Date.now()}_${Math.random().toString(36).substr(2, 9)}_${i}`;
+    ids.push(id);
+  }
+  return ids;
+};
