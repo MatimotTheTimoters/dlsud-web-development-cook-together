0af97a52c008733f5a40cd00f676706d6613d7a8
Raphael2334 <johnraphaelreganit23@gmail.com>
2025-12-07 17:43:44 +0800

    removed authprovider and added dataprovider and authcontext

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import { AuthProvider } from './hooks/useAuth.js';
+import { DataProvider } from './contexts/DataContext';
+import { AuthContext } from './contexts/AuthContext';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
+    <AuthContext>
+    <DataProvider>
       <App />
-    </AuthProvider>
+    </DataProvider>
+    </AuthContext>
   </React.StrictMode>
 );
