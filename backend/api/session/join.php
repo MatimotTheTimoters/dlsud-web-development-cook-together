bb2d844487a4fbaea4be30495014dc847ae1fa75
Raphael2334 <johnraphaelreganit23@gmail.com>
2025-12-15 19:52:09 +0800

    CORS Header

--- backend/api/session/join.php
+++ backend/api/session/join.php
@@ -1,5 +1,11 @@
 <?php
 require_once __DIR__ . '/../../db/connection.php';
+header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Credentials: true");
+
+echo json_encode(["success" => true, "message" => "API is working!"]);
 
 $db = new Database();
 $conn = $db->getConnection();
